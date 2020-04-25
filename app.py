a06f802685e0bc26c08a375217d13d69e39ab53e
KirillPanin <panin.kirill@gmail.com>
2020-04-26 00:52:35 +0300

    flaskapp

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, Response, render_template
+from generate_users import fake_people
+from spacemen import curr_spacemen
+from params import hei_wei
+from parser import parse_usr_value, parse_hum_params
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/owner/')
+def owner():
+    owner_name = "Panin Kirill"
+    return render_template('index.html', message=owner_name)
+
+
+@app.route('/generate-users/')
+def users():
+    usr_value = parse_usr_value(request.args.get('usr_value', '100'))
+    if type(usr_value) is str:
+        return Response(usr_value)
+    response = Response(fake_people(usr_value))
+    return response
+
+
+@app.route('/mean/')
+def users_values():
+    """
+    Returns default data if value missing: avg height/weight for route /mean/
+    Output data could be changed according to the list of params:
+    available_params = '1: weight/height(AVG), 2: weight/height(MIN), 3: weight/height(MAX)'
+    In this case use route /mean/?hum_params=
+    """
+    hum_params = parse_hum_params(request.args.get('hum_params', '1'))
+    if type(hum_params) is str:
+        return Response(hum_params)
+    response = Response(hei_wei(hum_params))
+    return response
+
+
+@app.route('/space/')
+def spacemen():
+    response = Response(curr_spacemen())
+    return response
+
+
+@app.route('/requirements/')
+def read_data():
+    with open('req.txt', encoding='utf-8') as f2:
+        data = f2.read()
+        f2.close()
+        return data
+
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run()
---
+++ parser.py
@@ -0,0 +1,24 @@
+def parse_usr_value(usr_value: str) -> int or str:
+    if not usr_value.isdigit():
+        return f'Incorrect value: {usr_value}. The request must contain a number between [1, 150]'
+
+    usr_value = int(usr_value)
+
+    if usr_value > 150 or usr_value < 1:
+        return f'Incorrect value: {usr_value}. Enter number in range [1, 150]'
+
+    return usr_value
+
+
+def parse_hum_params(hum_params: str) -> int or str:
+    available_params = '1: weight/height(AVG), 2: weight/height(MIN), 3: weight/height(MAX)'
+    if not hum_params.isdigit():
+        return f'Incorrect input: {hum_params}. Available requests: {available_params}. ' \
+               f'Chose argument between 1-3'
+
+    hum_params = int(hum_params)
+
+    if hum_params > 3 or hum_params < 1:
+        return f'Incorrect value: {hum_params}. Enter number in range [1, 3]'
+
+    return hum_params
