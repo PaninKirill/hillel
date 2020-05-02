fc72a29d780b99c5be38d510e4f957d85073c955
KirillPanin <panin.kirill@gmail.com>
2020-05-02 13:39:57 -0700

    django homework

--- src/students_tracker/urls.py
+++ src/students_tracker/urls.py
@@ -1,26 +1,35 @@
-"""students_tracker URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from students import views
 
+from students import views as s_views
+from teachers import views as t_views
+from groups import views as g_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('hello-world/', views.hello_world),
-    path('students/', views.students),
+    path('main/st/', s_views.students_app),
+    path('main/tc/', t_views.teacher_app),
+    path('main/gr/', g_views.group_app),
+    path('students/st/', s_views.students),
+    path('teachers/tc/', t_views.teachers),
+    path('groups/gr/', g_views.groups),
+    path('generate_student/st/', s_views.generate_student),
+    path('generate_teacher/tc/', t_views.generate_teacher),
+    path('generate_group/gr/', g_views.generate_group),
+    path('generate_students/st/', s_views.generate_students),
+    path('generate_teachers/tc/', t_views.generate_teachers),
+    path('generate_groups/gr/', g_views.generate_groups),
+    path('clear_sheet/st/', s_views.clear_sheet),
+    path('clear_sheet/tc/', t_views.clear_sheet),
+    path('clear_sheet/gr/', g_views.clear_sheet),
+    path('sheets/st/', s_views.sheets),
+    path('sheets/tc/', t_views.sheets),
+    path('sheets/gr/', g_views.sheets),
+    path('filter/st/', s_views.query_filter),
+    path('filter/tc/', t_views.query_filter),
+    path('filter/gr/', g_views.query_filter),
+    path('del_row/st/', s_views.delete_row),
+    path('del_row/tc/', t_views.delete_row),
+    path('del_row/gr/', g_views.delete_row),
 ]
