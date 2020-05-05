a534d3ed3532a6d987de399b4163f136c24960a0
KirillPanin <panin.kirill@gmail.com>
2020-05-05 04:22:48 -0700

    HW filter/command

--- src/students/models.py
+++ src/students/models.py
@@ -1,7 +1,26 @@
+import random
+
 from django.db import models
+
+from faker import Faker
 
 
 class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+    ]
+    grade = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     age = models.PositiveSmallIntegerField()  # models.IntegerField
@@ -11,8 +30,25 @@ class Student(models.Model):
         return f'{self.first_name} {self.last_name}'
 
     def info(self) -> str:
-        return f'{self.id} {self.first_name} {self.last_name} {self.age}'
+        return f'{self.id} {self.first_name} {self.last_name} {self.age} {self.grade}'
 
     def inc_age(self) -> None:
         self.age += 1
         self.save()
+
+    def __str__(self):
+        return f'{self.id}, {self.full_name}'
+
+    def __repr__(self):
+        return f'{self.id}, {self.full_name}, {self.grade}'
+
+    @classmethod
+    def add_student(cls):
+        fake = Faker()
+        student = cls(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            age=random.randint(18, 25),
+        )
+        student.save()
+        return student
